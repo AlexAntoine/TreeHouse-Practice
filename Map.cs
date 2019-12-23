c6cb2a034975666bacd674976440b0e693aa17b4
macbook <macbook@macbooks-MacBook-Pro-3.local>
2019-12-23 02:34:03 -0500

    added point and map class. initalize their attributes

--- Map.cs
+++ Map.cs
@@ -1,6 +1,15 @@
-namespace reeHouse_Practice {
+namespace TreeHouse_Practice {
 
     class Map {
+
+        public readonly int _width;
+        public readonly int _heigth;
+
+        public Map(int width, int heigth)
+        {
+            width = _width;
+            heigth = _heigth;
+        }
         
     }
 }
---
+++ Point.cs
@@ -0,0 +1,14 @@
+namespace TreeHouse_Practice {
+
+    class Point{
+
+        publit readonly int _x;
+        public readonly int _y;
+
+        public Point (int x, int y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
