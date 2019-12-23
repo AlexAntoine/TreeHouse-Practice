ea001bbb25c7f7b93599de77920a1a293e5af5c8
macbook <macbook@macbooks-MacBook-Pro-3.local>
2019-12-23 02:47:22 -0500

    added OnMap method. return a boolean if a point is placed on the map or not.

--- Map.cs
+++ Map.cs
@@ -3,12 +3,17 @@ namespace TreeHouse_Practice {
     class Map {
 
         public readonly int _width;
-        public readonly int _heigth;
+        public readonly int _height;
 
-        public Map(int width, int heigth)
+        public Map(int width, int height)
         {
             width = _width;
-            heigth = _heigth;
+            heigth = _height;
+        }
+
+        public bool OnMap(Point point)
+        {
+           return point._x >=0 && point._x < _width && point._y >= 0 && point._y < _height;
         }
         
     }
