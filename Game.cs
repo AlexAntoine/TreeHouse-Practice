be3a9b1c85d0759a8952f79833257897f7eb7318
macbook <macbook@macbooks-MacBook-Pro-3.local>
2019-12-24 22:56:04 -0500

    modify the game class

--- Game.cs
+++ Game.cs
@@ -8,9 +8,21 @@ namespace TreeHouse_Practice
         {
            Map map = new Map(8, 5);
 
-           Point point = new Point(4,2);
+           Point x = new MapLocation(4,2);
+
+           Point p = x;
+
+           map.OnMap(new MapLocation(0,0));
           
-           Console.WriteLine(point.distanceTo(5,5));
+           Console.WriteLine(x.distanceTo(5,5));
+
+           Console.WriteLine(x is MapLocation);
+
+           Console.WriteLine(x is Point);
+
+           Point point = new Point(0,0);
+
+           Console.WriteLine( point is MapLocation);
         }
     }
 }
